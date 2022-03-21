17f292b1e9a3336b76dc885dbb852af427648407
yuyanjun <yuyanjun@zego.im>
2022-03-21 14:47:01 +0800

    update resources

--- lib/common/input/input_widget.dart
+++ lib/common/input/input_widget.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'package:zego_call_flutter/common/style/styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_gen/gen_l10n/zego_call_localizations.dart';
+
+const Color sendButtonColor = Color(0xff0055FF);
+Color sendButtonDisableColor = sendButtonColor.withOpacity(0.3);
+const Color textBackgroundColor = Color(0xffF7F7F8);
+const sendButtonText = TextStyle(
+  color: Colors.white,
+  fontSize: 13.0,
+);
 
 class InputWidget extends HookWidget {
   InputWidget({required this.tempEditController, Key? key}) : super(key: key);
@@ -47,7 +54,7 @@ class InputWidget extends HookWidget {
                       margin:
                           EdgeInsets.only(top: 15.h, right: 22.w, bottom: 15.h),
                       decoration: const BoxDecoration(
-                          color: StyleColors.roomMessageInputBgColor,
+                          color: textBackgroundColor,
                           borderRadius: BorderRadius.all(Radius.circular(12))),
                       alignment: Alignment.center,
                       child: TextField(
@@ -80,15 +87,16 @@ class InputWidget extends HookWidget {
                     child: Container(
                       decoration: BoxDecoration(
                           color: _areFieldsEmpty.value
-                              ? StyleColors.roomMessageSendButtonBgDisableColor
-                              : StyleColors.roomMessageSendButtonBgColor,
-                          borderRadius: const BorderRadius.all(Radius.circular(12))),
+                              ? sendButtonDisableColor
+                              : sendButtonColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(12))),
                       padding: const EdgeInsets.only(
                           left: 10, right: 10, top: 10, bottom: 10),
                       alignment: Alignment.center,
                       child: Text(
                         AppLocalizations.of(context)!.roomPageSendMessage,
-                        style: StyleConstant.roomMessageSendButtonText,
+                        style: sendButtonText,
                       ),
                     ),
                   ),
