11ff4c0c8b8c6495cbccbcb305de9ab1e1c4757b
yuyanjun <yuyanjun@zego.im>
2022-03-22 17:27:35 +0800

    open link of auth page

--- lib/page/auth/auth_gate.dart
+++ lib/page/auth/auth_gate.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 import 'package:zego_call_flutter/constants/zego_page_constant.dart';
 
-import '../../service/zego_user_service.dart';
+import '../../common/style/styles.dart';
+import 'auth_protocol_item.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
+
 class AuthGate extends StatefulWidget {
   // ignore: public_member_api_docs
   const AuthGate({Key? key}) : super(key: key);
@@ -64,21 +64,9 @@ class _AuthGateState extends State<AuthGate> {
                       const SizedBox(
                         height: 159 / 2,
                       ),
-                      RichText(
-                        text: TextSpan(
-                          style: Theme.of(context).textTheme.headline4,
-                          children: const [
-                            TextSpan(
-                              text: "ZE",
-                            ),
-                            TextSpan(
-                              text: "GO",
-                              style: const TextStyle(color: Colors.blue),
-                            ),
-                            TextSpan(text: "CLOUD")
-                          ],
-                        ),
-                      ),
+                      Image(
+                          image: const AssetImage(StyleIconUrls.authLogo),
+                          height: 61.h),
                       const SizedBox(
                         height: 58 / 2,
                       ),
@@ -143,7 +131,15 @@ class _AuthGateState extends State<AuthGate> {
                                         BorderRadius.all(Radius.circular(20)),
                                   ),
                                   child: TextButton(
-                                    child: const Text('Sign in with Google'),
+                                    child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Image(
+                                              image: const AssetImage(
+                                                  StyleIconUrls.authIconGoogle),
+                                              height: 45.h),
+                                          const Text('Log in with Google')
+                                        ]),
                                     onPressed: isPolicyCheck
                                         ? _signInWithGoogle
                                         : () {
@@ -156,53 +152,10 @@ class _AuthGateState extends State<AuthGate> {
                                 ),
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Checkbox(
-                            checkColor: Colors.white,
-                            value: isPolicyCheck,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                isPolicyCheck = value!;
-                              });
-                            },
-                          ),
-                          Flexible(
-                            child: RichText(
-                              text: TextSpan(
-                                style: Theme.of(context).textTheme.bodyText1,
-                                children: [
-                                  const TextSpan(
-                                    text:
-                                        "I have read and agree to ZEGO Call's ",
-                                  ),
-                                  TextSpan(
-                                    text: 'Terms of Service',
-                                    style: const TextStyle(color: Colors.blue),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        // TODO open link
-                                      },
-                                  ),
-                                  const TextSpan(text: ' & '),
-                                  TextSpan(
-                                    text: 'Privacy Policy',
-                                    style: const TextStyle(color: Colors.blue),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        // TODO open link
-                                      },
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
+                      SizedBox(height: 48.h),
+                      AuthProtocolItem(updatePolicyCheckState),
+                      SizedBox(
+                        height: 76.h,
                       ),
                     ],
                   ),
@@ -213,6 +166,12 @@ class _AuthGateState extends State<AuthGate> {
         ),
       ),
     );
+  }
+
+  void updatePolicyCheckState(bool value) {
+    setState(() {
+      isPolicyCheck = value;
+    });
   }
 
   Future<void> _signInWithGoogle() async {
